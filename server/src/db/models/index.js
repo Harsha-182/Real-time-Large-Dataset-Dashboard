aad1bc2642a6d56c180ed0f3df985ac0ebdba215
Harsha <97773381+Harsha-182@users.noreply.github.com>
2024-11-03 22:20:32 +0530

    Add migration logic to run on deploy

--- server/src/db/models/index.js
+++ server/src/db/models/index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
+const { Umzug, SequelizeStorage } = require('umzug');
 require('dotenv').config();
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
@@ -33,8 +34,45 @@ if (options.use_env_variable) {
     },
     logging: false,});
 } else {
-  sequelize = new Sequelize(options.database, options.username, options.password, options);
+  sequelize = new Sequelize(process.env.POSTGRES_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,});
 }
+
+const runMigrations = async () => {
+  const umzug = new Umzug({
+    migrations: { glob: 'migrations/*.js' },
+    storage: new SequelizeStorage({ sequelize }),
+    context: sequelize.getQueryInterface(),
+    logger: console,
+  });
+
+  // Run migrations if there are any pending
+  await umzug.up();
+};
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to PostgreSQL has been established successfully.');
+
+    // Run migrations
+    await runMigrations();
+    console.log('Migrations completed.');
+
+    // Your app's main code here
+
+  } catch (error) {
+    console.error('Unable to connect to the database or run migrations:', error);
+  }
+})();
 
 fs
   .readdirSync(__dirname)
