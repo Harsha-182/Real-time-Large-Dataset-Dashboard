d6936427625fb940786c65d041f064a655b89712
Harsha <97773381+Harsha-182@users.noreply.github.com>
2024-11-07 00:06:43 +0530

    Update app.js

--- server/src/api/app.js
+++ server/src/api/app.js
@@ -9,7 +9,7 @@ const { httpErrorHandler, passport } = require('./middlewares');
 
 const app = express();
 
-app.use(cors('*'));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
